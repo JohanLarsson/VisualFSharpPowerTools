fc30acdc02f7fdbda88dbcb54f4d922bad3292ad
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-28 02:24:07 +0200

    Rewrote BreadcrumbMarginProvider in F#.

---
+++ src/FSharp.Editing.VisualStudio/Navigation/BreadcrumbBarMarginProvider.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharp.Editing.VisualStudio.Navigation
+
+open System.ComponentModel.Composition;
+open Microsoft.VisualStudio.Text.Editor;
+open Microsoft.VisualStudio.Utilities;
+
+[<Export(typeof<IWpfTextViewMarginProvider>)>]
+[<Name(FSharp.Editing.VisualStudio.Constants.BreadcrumbBarMarginName)>]
+[<Order(After = PredefinedMarginNames.Left)>]
+[<MarginContainer(PredefinedMarginNames.Top)>]
+[<ContentType("F#")>]
+//[<TextViewRole(PredefinedTextViewRoles.PrimaryDocument)>]
+type BreadcrumbMarginProvider() =
+    interface IWpfTextViewMarginProvider with
+        member x.CreateMargin(wpfTextViewHost, _) =
+            new BreadcrumbBarMargin(wpfTextViewHost.TextView) :> _
